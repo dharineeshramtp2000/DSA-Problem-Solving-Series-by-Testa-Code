17354c110b8fe6e6e32733e995559ab11a87cc99
Dharineesh Ram T P <60029791+dharineeshramtp2000@users.noreply.github.com>
2022-07-08 19:11:40 +0530

    Program 19

---
+++ Programs/19. findAllAnagrams.py
@@ -0,0 +1,33 @@
+def findAllAnagrams(s, p):
+
+    targetFrequency = self.getFrequency(p)
+
+    targetLength = len(p)
+
+    currentFrequency = [0] * 26
+
+    startPositions = []
+
+    for idx, char in enumerate(s):
+
+        currentFrequency[ord(char) - ord("a")] += 1
+
+        if idx - targetLength >= 0:
+
+            currentFrequency[ord(s[idx - targetLength]) - ord("a")] -= 1
+
+        if currentFrequency == targetFrequency:
+            startPositions.append(idx - targetLength + 1)
+
+    return startPositions    
+
+
+
+
+def getFrequency(string):
+    counter = [0] * 26
+
+    for char in string:
+        counter[ord(char) - ord("a")] += 1
+
+    return counter
